1710b5446553d6f1aa1bc6f9dd8623189eabb52e
Dhruba Datta <dhrubadatta30@gmail.com>
2026-09-20 19:20:17 +0530

    Add new binary search method

--- Binary.java
+++ Binary.java
@@ -2,6 +2,27 @@ import java.util.Arrays;
 
 class BiSearch{
 
+    public static int rotatedSearch(int[] arr, int target){
+        int low=0, high=arr.length-1;
+
+        while(low<=high){
+            int mid=low+(high-low)/2;
+            if(arr[mid]==target) return mid;
+            else if(arr[mid]>arr[low]){
+                if(arr[low]==target) return low;
+                else if(arr[low]<target && target<arr[mid]) high=mid-1;
+                else low=mid+1;
+            }
+            else{
+                if(arr[high]==target) return high;
+                else if(arr[mid]<target && target<arr[high]) low=mid+1;
+                else high=mid-1;
+            }
+        }
+        return -1;
+
+    }
+
     public static int[] firstOcc(int[] arr, int target){
         int l=0,h=arr.length-1;
         int[] first={-1,-1};
@@ -47,8 +68,13 @@ class BiSearch{
 public class Binary{
      
     public static void main(String[] args) {
-        int[] arr={2,8,8,8,8,8,11,13};
-        int ans[]=BiSearch.firstOcc(arr,8);
-        System.out.println(Arrays.toString(ans)); 
+        // int[] arr={2,8,8,8,8,8,11,13};
+        // int ans[]=BiSearch.firstOcc(arr,8);
+        // System.out.println(Arrays.toString(ans)); 
+
+        int[] a={7,8,9,1,2,3,4,5,6};
+        int ind=BiSearch.rotatedSearch(a,8);
+        System.out.println(ind);
+
     }
 }
