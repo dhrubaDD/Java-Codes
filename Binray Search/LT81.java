d2d61be45872869e9dcbbc512c934d05bb444578
Dhruba Datta <dhrubadatta30@gmail.com>
2026-09-22 19:05:11 +0530

    Initial Commit

---
+++ Binray Search/LT81.java
@@ -0,0 +1,32 @@
+public class LT81 {
+    
+    public boolean search(int[] nums, int target) {
+        int low=0,high=nums.length-1;
+
+        while(low<=high){
+            int mid=low+(high-low)/2;
+            if(nums[mid]==target) return true;
+            else if(nums[mid]==nums[low] && nums[mid]== nums[high]){
+                high--;
+                low++;
+            }
+            else if(nums[low]<=nums[mid]){
+                if(nums[low]==target) return true;
+                else if(nums[low]<target && target<nums[mid]) high=mid-1;
+                else low=mid+1;
+            }
+            else if(nums[mid]<=nums[high]){
+                if(nums[high]==target) return true;
+                else if(nums[mid]<target && target<nums[high]) low=mid+1;
+                else high=mid-1;
+            }
+            else if(nums[mid]==nums[low] && nums[mid]== nums[high]){
+                high--;
+                low++;
+            }
+            
+        }
+        return false;
+    }
+}
+
