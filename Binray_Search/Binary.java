e2bf41a23ab6becbb67dd3b5b426e1f375659789
Dhruba Datta <dhrubadatta30@gmail.com>
2026-09-24 00:00:34 +0530

    Aggressive Cows

--- Binray_Search/Binary.java
+++ Binray_Search/Binary.java
@@ -1,6 +1,38 @@
 import java.util.*;
 
 class BiSearch{
+    // Aggressive Cows Problem
+    public static int aggressiveCows(int[] arr, int n){
+        int low=1;
+        int high = Arrays.stream(arr).max().getAsInt();
+        int maxi=1;
+
+        Arrays.sort(arr);
+
+        while(low<=high){
+            int mid=(low+high)/2;
+            boolean posCow= placeCow(arr,mid,n);
+            if(posCow){
+                maxi=Math.max(maxi,mid);
+                low=mid+1;
+            }
+            else high=mid-1;
+        }
+        return maxi;
+    }
+    public static boolean placeCow(int[] arr, int m, int n){
+        int cow=1,last=0;
+        for(int i=1;i<arr.length;i++){
+            if(arr[i]-arr[last]>=m){
+                cow++;last=i;
+            }
+            else continue;
+        }
+        if(cow>=n) return true;
+        return false;
+    }
+
+
 
     // Blooming Flower problem
     public static int bloomDay(int[] arr, int m,int n){
@@ -125,8 +157,12 @@ public class Binary{
         // int ans=BiSearch.nthRoot(64,3);
         // System.out.println(ans);
 
-        int[]  arr={7,7,7,12,10,10,15,13};
-        int ans=BiSearch.bloomDay(arr,3,2);
+        // int[]  arr={7,7,7,12,10,10,15,13};
+        // int ans=BiSearch.bloomDay(arr,3,2);
+        // System.out.println(ans);
+
+        int[] arr={0,3,4,9,7,10};
+        int ans=BiSearch.aggressiveCows(arr,2);
         System.out.println(ans);
 
     }
